8a3e1b9f0364a7a15053d434c901458ca987a6cf
Leonard Tan <lentan1029@gmail.com>
2025-10-12 00:27:57 +0800

    add deploy script, remove unused ignition modules

--- ignition/modules/IdentityRegistry.ts
+++
@@ -1,9 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-const IdentityRegistryModule = buildModule("IdentityRegistryModule", (m) => {
-  const identityRegistry = m.contract("IdentityRegistry");
-  return { identityRegistry };
-});
-
-export default IdentityRegistryModule;
-
